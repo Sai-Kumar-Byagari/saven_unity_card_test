481e66d119e21c141cfb0f0142ff409d4fdb5b1d
Sai-Kumar-Byagari <saikumar.byagari@saven.in>
2024-05-03 11:57:48 +0530

    version 0.0.3

--- lib/main.dart
+++ lib/main.dart
@@ -5,35 +5,71 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _showWelcomeScreen = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MPinLoginPage(),
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: const Text('Welcome to Home'),
+        // ),
+        body: _showWelcomeScreen
+            ? const WelcomeScreen()
+            : const MPinLoginPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _showWelcomeScreen ? 0 : 1,
+          onTap: (index) {
+            setState(() {
+              _showWelcomeScreen = index == 0;
+            });
+          },
+          backgroundColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card),
+              label: 'Card',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            'Welcome to Travel App!!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ],
+      ),
     );
   }
 }
